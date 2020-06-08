47c4d01688772eab06379ea86a6dcddfb2ece4be
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-08 22:02:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,5 @@
 
 def square_array(array)
-  squared_array = []
-  size = array.length
-  index = 0
-  while index <= size {
-    squared_array[index] = array[index] * array[index]
-  }
+
   end
 end
