8e3424625dbbb52c397003cdcf4ebfe964f2232e
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-08 22:23:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,8 +5,8 @@ def square_array(array)
   new_array = []
   size = array.length
   while i <= size do
-    num = array[i]
-    new_array.push(num + 1)
+    num = array[i] * array[i]
+    new_array.push(num)
     i += 1
   end
   new_array
